c5b23b5f3cd5f84cd5d136ebb5b52ca0fc3d7a9a
mehdi <mehdih.devapp@gmail.com>
2023-06-14 16:59:40 +0200

    feat (#12): Controller and Authenticator for login route

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -17,31 +17,23 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Component\Security\Csrf\CsrfToken;
 use Symfony\Component\Security\Csrf\CsrfTokenManagerInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class AuthController extends AbstractController
 {
     /**
-     * Register new user
-     *
-     * @param Request $request
-     * @param UserPasswordHasherInterface $passwordHasher
-     * @param EntityManagerInterface $em
-     * @param MailerInterface $mailer
-     * @param CsrfTokenManagerInterface $csrfTokenManager
-     *
-     * @return Response
+     * Register new user.
      *
      * @throws TransportExceptionInterface
      */
-    #[Route('/auth/register', name: 'app_auth_register', methods: ["GET", "POST"])]
+    #[Route('/auth/register', name: 'app_auth_register', methods: ['GET', 'POST'])]
     public function register(
         Request $request,
         UserPasswordHasherInterface $passwordHasher,
         EntityManagerInterface $em,
         MailerInterface $mailer,
         CsrfTokenManagerInterface $csrfTokenManager
-    ): Response
-    {
+    ): Response {
         if ($this->getUser()) {
             return $this->redirectToRoute('home');
         }
@@ -56,44 +48,44 @@ class AuthController extends AbstractController
                 ->setUsername(
                     strtolower($user->getUsername())
                 )
-                ->setCreatedAt(new \DateTimeImmutable("now"))
+                ->setCreatedAt(new \DateTimeImmutable('now'))
                 ->setPassword(
                     $passwordHasher->hashPassword(
                         $user,
                         $form->get('repeatedPassword')->getData()
                     )
                 )
-                ->setValidEmailExpiredAt(new \DateTimeImmutable("+5 minutes"));
+                ->setValidEmailExpiredAt(new \DateTimeImmutable('+5 minutes'));
 
-            $token = uniqid("tk_");
+            $token = uniqid('tk_');
             $hash = $csrfTokenManager->getToken($token)->getValue();
             $user->setValidEmailToken($hash);
 
             $em->persist($user);
             $em->flush();
 
-            $url = $this->generateUrl("app_auth_email_confirmation", [
-                "username" => $user->getUsername(),
-                "token" => $token
+            $url = $this->generateUrl('app_auth_email_confirmation', [
+                'username' => $user->getUsername(),
+                'token' => $token,
             ], UrlGeneratorInterface::ABSOLUTE_URL);
 
             $email = (new TemplatedEmail())
                 ->to($user->getEmail())
-                ->subject("[P6] Bienvenue sur Snowtricks - Veuillez confirmer votre adresse email")
-                ->htmlTemplate("emails/register.html.twig")
+                ->subject('[P6] Bienvenue sur Snowtricks - Veuillez confirmer votre adresse email')
+                ->htmlTemplate('emails/register.html.twig')
                 ->context([
-                    "username" => $user->getUsername(),
-                    "url" => $url
+                    'username' => $user->getUsername(),
+                    'url' => $url,
                 ]);
 
             $mailer->send($email);
 
-            $this->addFlash("success",
-                "Votre compte a bien été créé, veuillez valider votre adresse email afin de vous connecter
-                sur le site et ainsi profiter de toutes les fonctionnalités de Snowtricks."
+            $this->addFlash('success',
+                'Votre compte a bien été créé, veuillez valider votre adresse email afin de vous connecter
+                sur le site et ainsi profiter de toutes les fonctionnalités de Snowtricks.'
             );
 
-            return $this->redirectToRoute("home");
+            return $this->redirectToRoute('home');
         }
 
         return $this->render('auth/register.html.twig', [
@@ -103,23 +95,15 @@ class AuthController extends AbstractController
 
     /**
      * Confirmation email with token
-     * or refresh it
-     *
-     * @param string|null $username
-     * @param string|null $token
-     * @param EntityManagerInterface $em
-     * @param MailerInterface $mailer
-     * @param CsrfTokenManagerInterface $csrfTokenManager
-     *
-     * @return Response
+     * or refresh it.
      *
      * @throws TransportExceptionInterface
      */
     #[Route(
         '/auth/email-confirmation/{username}_{token}',
         name: 'app_auth_email_confirmation',
-        requirements: ["username" => "[a-zA-Z0-9]+", "token" => "(tk_){1}[a-z0-9]+"],
-        methods: ["GET"]
+        requirements: ['username' => '[a-zA-Z0-9]+', 'token' => '(tk_){1}[a-z0-9]+'],
+        methods: ['GET']
     )]
     public function emailConfirmation(
         ?string $username,
@@ -127,13 +111,12 @@ class AuthController extends AbstractController
         EntityManagerInterface $em,
         MailerInterface $mailer,
         CsrfTokenManagerInterface $csrfTokenManager
-    ): Response
-    {
+    ): Response {
         $user = null;
 
         if ($username) {
             $user = $em->getRepository(User::class)->findOneBy([
-                "username" => $username
+                'username' => $username,
             ]);
         }
 
@@ -142,7 +125,7 @@ class AuthController extends AbstractController
         }
 
         if (
-            new \DateTimeImmutable("now") < $user->getValidEmailExpiredAt() &&
+            new \DateTimeImmutable('now') < $user->getValidEmailExpiredAt() &&
             $csrfTokenManager->isTokenValid(new CsrfToken($token, $user->getValidEmailToken()))
         ) {
             $user
@@ -153,97 +136,114 @@ class AuthController extends AbstractController
 
             $email = (new TemplatedEmail())
                 ->to($user->getEmail())
-                ->subject("[P6] Snowtricks - Votre compte est activé !")
-                ->htmlTemplate("emails/confirm-email.html.twig",)
+                ->subject('[P6] Snowtricks - Votre compte est activé !')
+                ->htmlTemplate('emails/confirm-email.html.twig')
                 ->context([
-                    "username" => $user->getUsername()
+                    'username' => $user->getUsername(),
                 ]);
 
             $mailer->send($email);
 
             $this->addFlash(
-                "success",
-                "Votre compte a bien été activé, vous pouvez vous connecter au site et profiter
-                de toutes les fonctionnalités de Snowtricks."
+                'success',
+                'Votre compte a bien été activé, vous pouvez vous connecter au site et profiter
+                de toutes les fonctionnalités de Snowtricks.'
             );
 
-            return $this->redirectToRoute("home");
+            return $this->redirectToRoute('home');
         }
 
         $this->addFlash(
-            "warning",
+            'warning',
             "Le lien de confirmation n'est plus valide, suivez la procédure de récupération d'un nouveau
             lien d'activation pour votre compte."
         );
 
-        return $this->redirectToRoute("app_auth_email_refresh_link");
+        return $this->redirectToRoute('app_auth_email_refresh_link');
     }
 
     /**
-     * Send new email with refresh link
+     * Send new email with refresh link.
      *
-     * @param Request $request
-     * @param EntityManagerInterface $em
-     * @param MailerInterface $mailer
-     * @param CsrfTokenManagerInterface $csrfTokenManager
-     *
-     * @return Response
      * @throws TransportExceptionInterface
      */
     #[Route(
         '/auth/email-refresh-link',
         name: 'app_auth_email_refresh_link',
-        methods: ["GET", "POST"]
+        methods: ['GET', 'POST']
     )]
     public function emailRefreshLink(
         Request $request,
         EntityManagerInterface $em,
         MailerInterface $mailer,
         CsrfTokenManagerInterface $csrfTokenManager
-    ): Response
-    {
+    ): Response {
         $form = $this->createForm(RefreshConfirmEmailType::class);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
             $user = $em->getRepository(User::class)->findOneBy([
-                "email" => $form->get('email')->getData()
+                'email' => $form->get('email')->getData(),
             ]);
 
             if ($user) {
-                $token = uniqid("tk_");
+                $token = uniqid('tk_');
                 $hash = $csrfTokenManager->getToken($token)->getValue();
 
                 $user
                     ->setValidEmailToken($hash)
-                    ->setValidEmailExpiredAt(new \DateTimeImmutable("+5 minutes"));
+                    ->setValidEmailExpiredAt(new \DateTimeImmutable('+5 minutes'));
 
                 $em->flush();
 
-                $url = $this->generateUrl("app_auth_email_confirmation", [
-                    "username" => $user->getUsername(),
-                    "token" => $token
+                $url = $this->generateUrl('app_auth_email_confirmation', [
+                    'username' => $user->getUsername(),
+                    'token' => $token,
                 ], UrlGeneratorInterface::ABSOLUTE_URL);
 
                 $email = (new TemplatedEmail())
                     ->to($user->getEmail())
-                    ->subject("[P6] Snowtricks - Lien de confirmation de votre compte expiré !")
-                    ->htmlTemplate("emails/refresh-link-confirm-email.html.twig")
+                    ->subject('[P6] Snowtricks - Lien de confirmation de votre compte expiré !')
+                    ->htmlTemplate('emails/refresh-link-confirm-email.html.twig')
                     ->context([
-                        "username" => $user->getUsername(),
-                        "url" => $url
+                        'username' => $user->getUsername(),
+                        'url' => $url,
                     ]);
 
                 $mailer->send($email);
             }
 
-            $this->addFlash("success", "Votre lien de confirmation a bien été envoyé.");
+            $this->addFlash('success', 'Votre lien de confirmation a bien été envoyé.');
 
-            return $this->redirectToRoute("home");
+            return $this->redirectToRoute('home');
         }
 
         return $this->render('auth/email-refresh-confirmation.html.twig', [
             'form' => $form->createView(),
         ]);
+    }
+
+    #[Route(path: '/auth/login', name: 'app_auth_login', methods: ['GET', 'POST'])]
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+         if ($this->getUser()) {
+             return $this->redirectToRoute('home');
+         }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('auth/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error
+        ]);
+    }
+
+    #[Route(path: '/auth/logout', name: 'app_auth_logout')]
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
