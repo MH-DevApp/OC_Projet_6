c82d4ceeedb3d2fd6c60ab891ce4655bce7c801e
mehdi <mehdih.devapp@gmail.com>
2023-06-14 22:25:05 +0200

    fix: rename RefreshConformEmailType FormType to EmailFormType for use in Confirmation email and forgotten token

--- src/Form/EmailFormType.php
+++ src/Form/EmailFormType.php
@@ -9,7 +9,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Email;
 
-class RefreshConfirmEmailType extends AbstractType
+class EmailFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
