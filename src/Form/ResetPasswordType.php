1d026225a6e37386977fda2b373376763359f120
mehdi <mehdih.devapp@gmail.com>
2023-06-15 22:22:47 +0200

    feat (#14): Reset Password FormType

---
+++ src/Form/ResetPasswordType.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+
+class ResetPasswordType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        if (isset($options["data"]) && $options["data"]["type"] === "profile") {
+            $builder
+                ->add("actualPassword", PasswordType::class, [
+                    "mapped" => false,
+                    "label" => "Mot de passe actuel",
+                    "label_attr" => [
+                        "class" => "form-label"
+                    ],
+                    "row_attr" => [
+                        "class" => "mb-3"
+                    ],
+                    "attr" => [
+                        "class" => "form-control"
+                    ],
+                    "required" => true
+                ]);
+        }
+
+        $builder
+            ->add("repeatedPassword", RepeatedType::class, [
+                "type" => PasswordType::class,
+                "mapped" => false,
+                "invalid_message" => "Les mots de passe ne sont pas identiques.",
+                "first_options" => [
+                    "label" => "Nouveau mot de passe",
+                    "label_attr" => [
+                        "class" => "form-label"
+                    ],
+                    "row_attr" => [
+                        "class" => "mb-3"
+                    ],
+                    "attr" => [
+                        "class" => "form-control"
+                    ],
+                ],
+                "second_options" => [
+                    "label" => "Confirmer le nouveau mot de passe",
+                    "label_attr" => [
+                        "class" => "form-label"
+                    ],
+                    "row_attr" => [
+                        "class" => "mb-3"
+                    ],
+                    "attr" => [
+                        "class" => "form-control"
+                    ],
+                ],
+                "required" => true,
+                "constraints" => [
+                    new Length([
+                        "min" => 6,
+                        "max" => 20,
+                        "minMessage" => "Votre mot de passe doit contenir au moins {{ limit }} caractères.",
+                        "maxMessage" => "Votre mot de passe doit contenir au plus {{ limit }} caractères.",
+                    ])
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            // Configure your form options here
+            'attr' => [
+                "class" => "w-100 shadow p-3 mb-5 rounded"
+            ]
+        ]);
+    }
+}
