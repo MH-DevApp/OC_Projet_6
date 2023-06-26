2ff092b2bdd331e8b582cad55e30d5062f159343
Mehdi H <78570781+MH-DevApp@users.noreply.github.com>
2023-06-26 21:43:24 +0200

    feat (#39): homepage feature (#17)
    * feat (#17): images of catch phrase x3
    
    * feat (#17): styles files for homepage
    
    * feat (#17): view file of homepage
    
    * feat (#17): homepage controller feature
    
    * fix: update project to symfony v5.4.25

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Trick;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,8 +11,20 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route("/", name: "home", methods: ["GET"])]
-    public function index(): Response
+    public function index(
+        EntityManagerInterface $entityManager
+    ): Response
     {
-        return $this->render('home/index.html.twig');
+        $tricks = $entityManager->getRepository(Trick::class)->findBy(
+            [
+                'isPublished' => true
+            ], [
+                'createdAt' => 'DESC'
+            ]
+        );
+
+        return $this->render('home/index.html.twig', [
+            "tricks" => $tricks
+        ]);
     }
 }
