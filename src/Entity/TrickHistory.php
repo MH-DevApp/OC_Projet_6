0b62877ca734fbb3ef693ab3b77fbaded844dd20
mehdi <mehdih.devapp@gmail.com>
2023-07-10 23:11:37 +0200

    fix : change all structure to trick history entity with logs only

--- src/Entity/TrickHistory.php
+++ src/Entity/TrickHistory.php
@@ -19,20 +19,8 @@ class TrickHistory
     #[ORM\Column(type: 'uuid', unique: true)]
     private ?UuidV6 $id = null;
 
-    #[ORM\Column(length: 255, unique: true)]
-    private ?string $name = null;
-
-    #[ORM\Column(type: Types::TEXT)]
-    private ?string $description = null;
-
     #[ORM\Column]
     private ?\DateTimeImmutable $updatedAt = null;
-
-    #[ORM\OneToOne(cascade: ['persist', 'remove'])]
-    private ?MediaTrick $pictureFeatured = null;
-
-    #[ORM\ManyToMany(targetEntity: MediaTrick::class, inversedBy: 'trickHistories', cascade: ['persist', 'remove'])]
-    private Collection $mediasTrick;
 
     #[ORM\ManyToOne(cascade: ['persist'], inversedBy: 'trickHistories')]
     #[ORM\JoinColumn(nullable: false)]
@@ -40,44 +28,41 @@ class TrickHistory
 
     #[ORM\ManyToOne(cascade: ['persist'])]
     #[ORM\JoinColumn(nullable: false)]
-    private ?GroupTrick $groupTrick = null;
-
-    #[ORM\ManyToOne(cascade: ['persist'])]
-    #[ORM\JoinColumn(nullable: false)]
     private ?User $author = null;
 
-    public function __construct()
-    {
-        $this->mediasTrick = new ArrayCollection();
-    }
+    #[ORM\Column]
+    private ?bool $isNameUpdated = false;
+
+    #[ORM\Column]
+    private ?bool $isDescriptionUpdated = false;
+
+    #[ORM\Column]
+    private ?bool $isGroupTrickUpdated = false;
+
+    #[ORM\Column]
+    private ?bool $isMediaFeatured = false;
+
+    #[ORM\Column]
+    private ?bool $isMediaImageAdded = false;
+
+    #[ORM\Column]
+    private ?bool $isMediaVideoAdded = false;
+
+    #[ORM\Column]
+    private ?bool $isMediaImageUpdated = false;
+
+    #[ORM\Column]
+    private ?bool $isMediaVideoUpdated = false;
+
+    #[ORM\Column]
+    private ?bool $isMediaImageDeleted = false;
+
+    #[ORM\Column]
+    private ?bool $isMediaVideoDeleted = false;
 
     public function getId(): ?UuidV6
     {
         return $this->id;
-    }
-
-    public function getName(): ?string
-    {
-        return $this->name;
-    }
-
-    public function setName(string $name): self
-    {
-        $this->name = $name;
-
-        return $this;
-    }
-
-    public function getDescription(): ?string
-    {
-        return $this->description;
-    }
-
-    public function setDescription(string $description): self
-    {
-        $this->description = $description;
-
-        return $this;
     }
 
     public function getUpdatedAt(): ?\DateTimeImmutable
@@ -88,42 +73,6 @@ class TrickHistory
     public function setUpdatedAt(\DateTimeImmutable $updatedAt): self
     {
         $this->updatedAt = $updatedAt;
-
-        return $this;
-    }
-
-    public function getPictureFeatured(): ?MediaTrick
-    {
-        return $this->pictureFeatured;
-    }
-
-    public function setPictureFeatured(?MediaTrick $pictureFeatured): self
-    {
-        $this->pictureFeatured = $pictureFeatured;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection<int, MediaTrick>
-     */
-    public function getMediasTrick(): Collection
-    {
-        return $this->mediasTrick;
-    }
-
-    public function addMediasTrick(MediaTrick $mediasTrick): self
-    {
-        if (!$this->mediasTrick->contains($mediasTrick)) {
-            $this->mediasTrick->add($mediasTrick);
-        }
-
-        return $this;
-    }
-
-    public function removeMediasTrick(MediaTrick $mediasTrick): self
-    {
-        $this->mediasTrick->removeElement($mediasTrick);
 
         return $this;
     }
@@ -140,18 +89,6 @@ class TrickHistory
         return $this;
     }
 
-    public function getGroupTrick(): ?GroupTrick
-    {
-        return $this->groupTrick;
-    }
-
-    public function setGroupTrick(?GroupTrick $groupTrick): self
-    {
-        $this->groupTrick = $groupTrick;
-
-        return $this;
-    }
-
     public function getAuthor(): ?User
     {
         return $this->author;
@@ -160,6 +97,126 @@ class TrickHistory
     public function setAuthor(?User $author): self
     {
         $this->author = $author;
+
+        return $this;
+    }
+
+    public function isIsNameUpdated(): ?bool
+    {
+        return $this->isNameUpdated;
+    }
+
+    public function setIsNameUpdated(bool $isNameUpdated): static
+    {
+        $this->isNameUpdated = $isNameUpdated;
+
+        return $this;
+    }
+
+    public function isIsDescriptionUpdated(): ?bool
+    {
+        return $this->isDescriptionUpdated;
+    }
+
+    public function setIsDescriptionUpdated(bool $isDescriptionUpdated): static
+    {
+        $this->isDescriptionUpdated = $isDescriptionUpdated;
+
+        return $this;
+    }
+
+    public function isIsGroupTrickUpdated(): ?bool
+    {
+        return $this->isGroupTrickUpdated;
+    }
+
+    public function setIsGroupTrickUpdated(bool $isGroupTrickUpdated): static
+    {
+        $this->isGroupTrickUpdated = $isGroupTrickUpdated;
+
+        return $this;
+    }
+
+    public function isIsMediaFeatured(): ?bool
+    {
+        return $this->isMediaFeatured;
+    }
+
+    public function setIsMediaFeatured(bool $isMediaFeatured): static
+    {
+        $this->isMediaFeatured = $isMediaFeatured;
+
+        return $this;
+    }
+
+    public function isIsMediaImageAdded(): ?bool
+    {
+        return $this->isMediaImageAdded;
+    }
+
+    public function setIsMediaImageAdded(bool $isMediaImageAdded): static
+    {
+        $this->isMediaImageAdded = $isMediaImageAdded;
+
+        return $this;
+    }
+
+    public function IsMediaVideoAdded(): ?bool
+    {
+        return $this->isMediaVideoAdded;
+    }
+
+    public function setIsMediaVideoAdded(bool $isMediaVideoAdded): static
+    {
+        $this->isMediaVideoAdded = $isMediaVideoAdded;
+
+        return $this;
+    }
+
+    public function isIsMediaImageUpdated(): ?bool
+    {
+        return $this->isMediaImageUpdated;
+    }
+
+    public function setIsMediaImageUpdated(bool $isMediaImageUpdated): static
+    {
+        $this->isMediaImageUpdated = $isMediaImageUpdated;
+
+        return $this;
+    }
+
+    public function isIsMediaVideoUpdated(): ?bool
+    {
+        return $this->isMediaVideoUpdated;
+    }
+
+    public function setIsMediaVideoUpdated(bool $isMediaVideoUpdated): static
+    {
+        $this->isMediaVideoUpdated = $isMediaVideoUpdated;
+
+        return $this;
+    }
+
+    public function isIsMediaImageDeleted(): ?bool
+    {
+        return $this->isMediaImageDeleted;
+    }
+
+    public function setIsMediaImageDeleted(bool $isMediaImageDeleted): static
+    {
+        $this->isMediaImageDeleted = $isMediaImageDeleted;
+
+        return $this;
+    }
+
+    public function isIsMediaVideoDeleted(): ?bool
+    {
+        return $this->isMediaVideoDeleted;
+    }
+
+    public function setIsMediaVideoDeleted(bool $isMediaVideoDeleted): static
+    {
+        $this->isMediaVideoDeleted = $isMediaVideoDeleted;
 
         return $this;
     }
